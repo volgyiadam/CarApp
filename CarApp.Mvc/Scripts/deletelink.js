e7dc0aab31b53379fd92110514d1f33a5ae9128a
volgyiadam <volgyiadam@gmail.com>
2014-08-15 06:45:14 +0200

    design

---
+++ CarApp.Mvc/Scripts/deletelink.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $(".DeleteLink").click(function (e) {
+        e.preventDefault();
+
+        var self = $(this);
+        var url = self.attr('href');
+        $.post(url, null, function (data) {
+            if (data.success) {
+                self.parent().parent().remove();
+            }
+            else {
+                alert("Hiba!");
+            }
+        }).fail(function () {
+            alert("Hiba!");
+        })
+    })
+});
